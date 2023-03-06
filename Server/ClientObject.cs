3434b83f9171ff26028fc53fd02e739866cc7e74
F1st3K <kostinnikita988@gmail.com>
2023-03-06 19:12:17 +0300

    first server

---
+++ Server/ClientObject.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Net.Sockets;
+
+namespace Server
+{
+    public class ClientObject
+    {
+        private TcpClient _tcpClient;
+        
+        public ClientObject(TcpClient tcpClient)
+        {
+            Id = Guid.NewGuid().ToString();
+            _tcpClient = tcpClient;
+        }
+
+        protected internal string Id { get;}
+        
+        protected internal string UserName { get; set; }
+
+
+        public void Close()
+        {
+            _tcpClient.Close();
+        }
+
+    }
+}
---
+++ Server/Server.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections.Generic;
+using System.IO;
+using System.Net;
+using System.Net.Sockets;
+using System.Threading.Tasks;
+
+namespace Server
+{
+    public class Server
+    {
+        private TcpListener _tcpListener;
+        private List<ClientObject> _clients;
+
+        public Server(TcpListener tcpListener)
+        {
+            _tcpListener = tcpListener;
+            _clients = new List<ClientObject>();
+        }
+
+        public async Task ListenAsync()
+        {
+            try
+            {
+                _tcpListener.Start();
+ 
+                while (true)
+                {
+                    TcpClient tcpClient = await _tcpListener.AcceptTcpClientAsync();
+                    var clientObject = new ClientObject(tcpClient);
+                    var reader = new StreamReader(tcpClient.GetStream());
+                    clientObject.UserName = await reader.ReadLineAsync();
+                    _clients.Add(clientObject);
+                }
+            }
+            finally
+            {
+                Disconnect();
+            }
+        }
+
+        private void Disconnect()
+        {
+            foreach (var client in _clients)
+            {
+                client.Close();
+            }
+            _tcpListener.Stop();
+        }
+    }
+}
